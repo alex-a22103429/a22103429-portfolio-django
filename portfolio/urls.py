856d1b024cdd18741898f2ae975dbde199638e70
alex-a22103429 <a22103429@alunos.ulht.pt>
2023-05-24 11:10:19 +0100

    yahallo

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,12 +5,7 @@ from . import views
 app_name = 'portfolio'
 
 urlpatterns = [
-    path('', views.index_view),
-    path('home', views.home_page_view, name='home'),
-    path('index', views.index_view),
-    path('products', views.products_view, name='products'),
-    path('about', views.about_view, name='about'),
-    path('contact', views.contact_view, name='contact'),
+    path('home', views.products_view, name='home'),
 ]
 #  hello/urls.py
 
